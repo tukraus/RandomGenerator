044c660912a7f4ffaf907021b3c5bfeb0fa15fc2
Natan Facchin <natan.facchin@hp.com>
2017-02-15 15:27:32 -0200

    Using secure random and optimized performance

--- Lib/Java/Sources/src/main/java/com/nfsindustries/luckyrandomgenerator/LuckyRandomGenerator.java
+++ Lib/Java/Sources/src/main/java/com/nfsindustries/luckyrandomgenerator/LuckyRandomGenerator.java
@@ -1,96 +1,103 @@
 //package com.nfsindustries.luckyrandomgenerator;
 
+import java.security.SecureRandom;
 import java.util.*;
 
 public class LuckyRandomGenerator {
-  public static int smallestAllowedValue = 1;
-  public static int biggestAllowedValueInUk = 59;
-  public static int biggestAllowedValueInBrazil = 60;
+    public static int smallestAllowedValue = 1;
+    public static int biggestAllowedValueInUk = 59;
+    public static int biggestAllowedValueInBrazil = 60;
 
-  private String saltchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!#$%&*()[];.,<>/{}-+_";
+    private String saltchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@!#$%&*()[];.,<>/{}-+_";
 
-  private int minIntValue = smallestAllowedValue;
-  private int maxIntValue;
-  private Random random = new Random();
+    private int minIntValue = smallestAllowedValue;
+    private int maxIntValue;
+    private SecureRandom random = new SecureRandom();
 
-  public LuckyRandomGenerator() {
-    maxIntValue = biggestAllowedValueInUk;
-  }
 
-  public LuckyRandomGenerator(int min, int max) {
-    minIntValue = min;
-    maxIntValue = max;
-  }
-
-  public LuckyRandomGenerator(int max) {
-    maxIntValue = max;
-  }
-
-  /**
-  * Rolled a fair dice, guaranteed to be random
-  */
-  public int generateRandomNumber(int min, int max) {
-    Random random = new Random();
-    return random.nextInt(max - min + 1) + min;
-  }
-
-  public Set generateLotteryTickets(int quantity) {
-    SortedSet<Integer> tickets = new TreeSet<Integer>();
-    while(tickets.size() < quantity) {
-      tickets.add(generateRandomNumber());
+    public LuckyRandomGenerator() {
+        maxIntValue = biggestAllowedValueInUk;
     }
-    return tickets;
-  }
 
-  public Set generateLotteryTickets(int quantity, int min, int max) {
-    minIntValue = min;
-    maxIntValue = max;
-    return generateLotteryTickets(quantity);
-  }
+    public LuckyRandomGenerator(final String locale) {
+        if (locale.equalsIgnoreCase("uk")) {
+            maxIntValue = biggestAllowedValueInUk;
+        }
+    }
 
-  public String generateRandomString(int length, String aSaltString) {
-    saltchars = aSaltString;
-    if (length < 1) {
-      throw new IllegalArgumentException("length < 1: " + length);
+    public LuckyRandomGenerator(int min, int max) {
+        minIntValue = min;
+        maxIntValue = max;
     }
-    StringBuilder salt = new StringBuilder();
-    while (salt.length() < length) {
-      int index = (int) (random.nextFloat() * saltchars.length());
-      salt.append(saltchars.charAt(index));
-    }
-    final String saltStr = salt.toString();
-    return saltStr;
-  }
 
-  public String generateRandomString(int length) {
-    if (length < 1) {
-      throw new IllegalArgumentException("length < 1: " + length);
+    public LuckyRandomGenerator(int max) {
+        maxIntValue = max;
     }
-    StringBuilder salt = new StringBuilder();
-    while (salt.length() < length) {
-      int index = (int) (random.nextFloat() * saltchars.length());
-      salt.append(saltchars.charAt(index));
-    }
-    String saltStr = salt.toString();
-    return saltStr;
-  }
 
-  public Set generateLotteryTickets(int quantity, int max) {
-    maxIntValue = max;
-    return generateLotteryTickets(quantity);
-  }
+    /**
+     * Rolled a fair dice, guaranteed to be random
+     */
+    public int generateRandomNumber(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    public Set<Integer> generateLotteryTickets(int quantity) {
+        SortedSet<Integer> tickets = new TreeSet<Integer>();
+        while(tickets.size() < quantity) {
+            tickets.add(generateRandomNumber());
+        }
+        return tickets;
+    }
+
+    public Set generateLotteryTickets(int quantity, int min, int max) {
+        minIntValue = min;
+        maxIntValue = max;
+        return generateLotteryTickets(quantity);
+    }
+
+    public String generateRandomString(int length, String aSaltString) {
+        saltchars = aSaltString;
+        if (length < 1) {
+            throw new IllegalArgumentException("length < 1: " + length);
+        }
+        StringBuilder salt = new StringBuilder();
+        while (salt.length() < length) {
+            int index = (int) (random.nextFloat() * saltchars.length());
+            salt.append(saltchars.charAt(index));
+        }
+        final String saltStr = salt.toString();
+        return saltStr;
+    }
+
+    public String generateRandomString(int length) {
+        if (length < 1) {
+            throw new IllegalArgumentException("length < 1: " + length);
+        }
+        StringBuilder salt = new StringBuilder();
+        while (salt.length() < length) {
+            int index = (int) (random.nextFloat() * saltchars.length());
+            salt.append(saltchars.charAt(index));
+        }
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+
+    public Set generateLotteryTickets(int quantity, int max) {
+        maxIntValue = max;
+        return generateLotteryTickets(quantity);
+    }
+
+    /**
+     * Rolled a fair dice, guaranteed to be random
+     */
+    public int generateRandomNumber() {
+        return generateRandomNumber(minIntValue, maxIntValue);
+    }
 
   public void printLotteryTickets(Set<Integer> tickets) {
     for (int currentTicket : tickets) {
       System.out.println("Ticket: " + String.valueOf(currentTicket));
     }
-  }
-
-  /**
-  * Rolled a fair dice, guaranteed to be random
-  */
-  public int generateRandomNumber() {
-    return generateRandomNumber(minIntValue, maxIntValue);
   }
 
   public static void main(final String... args) {
