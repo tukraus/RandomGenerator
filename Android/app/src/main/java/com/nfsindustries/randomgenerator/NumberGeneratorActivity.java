66d603dc4b77fd3e3e04c0d224f53f5889d317e6
Natan Facchin <maxmousee@gmail.com>
2017-02-16 16:29:30 -0200

    Added sample app

--- Android/app/src/main/java/com/nfsindustries/randomgenerator/NumberGeneratorActivity.java
+++ Android/app/src/main/java/com/nfsindustries/randomgenerator/NumberGeneratorActivity.java
@@ -1,8 +1,16 @@
 package com.nfsindustries.randomgenerator;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.Set;
 
@@ -17,14 +25,25 @@ public class NumberGeneratorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_generator);
-        generatedNumbersTextView = (TextView)findViewById(R.id.generatedNumbersTextView);
-        generatedStringTextView = (TextView)findViewById(R.id.generatedStringTextView);
+        generatedNumbersTextView = (TextView) findViewById(R.id.generatedNumbersTextView);
+        generatedStringTextView = (TextView) findViewById(R.id.generatedStringTextView);
+        animateViews();
+
         locale = this.getResources().getConfiguration().locale.getCountry();
         generator = new LuckyRandomGenerator(locale);
 
         Set<Integer> tickets = generator.generateLotteryTickets(6);
-        generatedNumbersTextView.setText(tickets.toString());
 
+        generatedNumbersTextView.setText(tickets.toString());
         generatedStringTextView.setText(generator.generateRandomString(8));
+    }
+
+    private void animateViews() {
+        Animation animation =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.blink);
+
+        generatedNumbersTextView.startAnimation(animation);
+        generatedStringTextView.startAnimation(animation);
     }
 }
