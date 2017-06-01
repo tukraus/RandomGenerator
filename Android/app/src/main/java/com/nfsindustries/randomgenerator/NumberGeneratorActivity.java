95f5b6798fb978c1ae2981d2ed718eebf6b888d3
Natan Facchin <maxmousee@gmail.com>
2017-06-01 14:41:01 -0400

    Added regenerate button - Android

--- Android/app/src/main/java/com/nfsindustries/randomgenerator/NumberGeneratorActivity.java
+++ Android/app/src/main/java/com/nfsindustries/randomgenerator/NumberGeneratorActivity.java
@@ -2,8 +2,10 @@ package com.nfsindustries.randomgenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Set;
@@ -13,6 +15,7 @@ public class NumberGeneratorActivity extends AppCompatActivity {
     private TextView generatedStringTextView;
     private String locale;
     private LuckyRandomGenerator generator;
+    private Button regenerateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,13 @@ public class NumberGeneratorActivity extends AppCompatActivity {
     private void setUpViews() {
         generatedNumbersTextView = (TextView) findViewById(R.id.generatedNumbersTextView);
         generatedStringTextView = (TextView) findViewById(R.id.generatedStringTextView);
+        regenerateButton = (Button) findViewById(R.id.regenerateButton);
+        regenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                generateRandom();
+            }
+        });
     }
 
     private void animateViews() {
