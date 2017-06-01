87cef1d0dffcf6c0c1b1c565821295160b9dbb53
Natan Facchin <maxmousee@gmail.com>
2017-06-01 10:54:47 -0400

    Added unit and instrumented tests for Android

---
+++ Android/app/src/test/java/com/nfsindustries/randomgenerator/LuckyNumberGeneratorTest.java
@@ -0,0 +1,34 @@
+package com.nfsindustries.randomgenerator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class LuckyNumberGeneratorTest {
+
+
+    @Test
+    public void generation_isNotNull() throws Exception {
+        LuckyRandomGenerator generator = new LuckyRandomGenerator();
+        assertNotNull(generator.generateRandomNumber());
+    }
+
+    @Test
+    public void generation_isInRangeDefault() throws Exception {
+        LuckyRandomGenerator generator = new LuckyRandomGenerator();
+        assertNotEquals(generator.generateRandomNumber(), 61);
+        assertNotEquals(generator.generateRandomNumber(), 0);
+    }
+
+    @Test
+    public void generation_isInRangeUK() throws Exception {
+        LuckyRandomGenerator generator = new LuckyRandomGenerator("uk");
+        assertNotEquals(generator.generateRandomNumber(), 60);
+        assertNotEquals(generator.generateRandomNumber(), 0);
+    }
+}
